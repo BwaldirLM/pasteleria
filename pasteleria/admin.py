c36cfe3d24263b4f9466e5d5b8246a6be9f6df84
BwaldirLM <bwald-lm@hotmail.com>
2022-07-18 16:37:43 -0500

    Agregar modelos

--- pasteleria/admin.py
+++ pasteleria/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
-from pasteleria.models import Pastel
+from pasteleria.models import *
 
 # Register your models here.
 
 class PastelAdmin(admin.ModelAdmin):
-    list_display = ['descripcion', 'precio']
+    list_display = ['nombre','descripcion', 'precio']
     ordering =  ['nombre']
 
-admin.site.register(Pastel)
+admin.site.register(Pastel, PastelAdmin)
+admin.site.register(Vendedor)
+admin.site.register(Produccion)
+admin.site.register(Cliente)
+admin.site.register(JefeProduccion)
+admin.site.register(Venta)
+admin.site.register(VentaDetalle)
+admin.site.register(Pago)
--- pasteleria/models.py
+++ pasteleria/models.py
@@ -1,12 +1,44 @@
 from calendar import c
 from django.db import models
+from django.contrib.auth.models import User
 
 TAMAÑOS = [
     ('P','PEQUEÑO'), 
     ('M','MEDIANO'),
     ('G','GRANDE')]
 
+PAGO = [
+    ('Tarjeta', 'TARJETA'),
+    ('Efectivo', 'EFECTIVO')
+]
+
 # Create your models here.
+
+class Persona(models.Model):
+    dni = models.CharField('DNI', max_length=8, unique=True)
+    telefono = models.CharField('Telefono', max_length=15)
+    direccion = models.TextField('Direccion')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+class JefeProduccion(Persona):
+    id = models.AutoField(primary_key=True)
+    sueldo = models.FloatField('Sueldo')
+    descripcion = models.TextField('Descripcion')
+
+class Vendedor(Persona):
+    id = models.AutoField(primary_key=True)
+    sueldo = models.FloatField('Sueldo')
+    descripcion = models.TextField('Descripcion')
+
+
+class Cliente(Persona):
+    id = models.AutoField(primary_key=True)
+    estado = models.BooleanField('Estado',default=True)
+    fecha_registro = models.DateField('Fecha de registro', auto_now_add=True)
+
 
 class Pastel(models.Model):
     id = models.AutoField(primary_key=True)
@@ -14,6 +46,10 @@ class Pastel(models.Model):
     descripcion = models.TextField('Descripcion')
     precio = models.FloatField('Precio')
     tamaño = models.CharField('Tamaño', choices= TAMAÑOS, max_length=10)
+
+    class Meta:
+        verbose_name = 'Pastel'
+        verbose_name_plural = 'Pasteles'
 
     def __str__(self) -> str:
         return self.nombre
@@ -24,3 +60,21 @@ class Produccion(models.Model):
     fecha = models.DateField('Fecha')
     cantidad = models.IntegerField('Cantidad')
     pastel = models.ForeignKey(Pastel, on_delete=models.CASCADE)
+
+class Venta(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateField('Fecha', auto_now_add=True)
+    importe = models.FloatField('Importe')
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name='Cliente')
+    Vendedor = models.ForeignKey(Vendedor, on_delete=models.CASCADE, verbose_name='Vemdedor')
+
+class VentaDetalle(models.Model):
+    id = models.AutoField(primary_key=True)
+    cantidad = models.IntegerField('Cantidad', default = 1)
+    pastel = models.ForeignKey(Pastel, on_delete=models.CASCADE)
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+
+class Pago(models.Model):
+    id = models.AutoField(primary_key=True)
+    forma_pago = models.CharField('Forma de pago', max_length=8, choices=PAGO)
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
